c167db05bf7e9d8da86e1f21f467ac0d95257d3e
Naandom Godson Longjel <nandomgodson@gmail.com>
2022-09-08 14:06:24 -0700

    fixed task five c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@
  * main - Entry point
  * 
  * Return: Always 0 (Success)
- */
+ **/
 int main(void)
 {
-  printf("with proper grammer, but the outcomr is a piece of art,\n");
-    return(0);
+printf("with proper grammer, but the outcomr is a piece of art,\n");
+return (0);
 }
